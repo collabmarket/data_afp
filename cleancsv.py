1fc43238f0a1249553ff0f98d18b80f103a4e0de
jrovegno <javier.rovegno@gmail.com>
2016-03-25 20:37:31 -0300

    cleancsv add main and comments

--- cleancsv.py
+++ cleancsv.py
@@ -11,11 +11,13 @@ yearcsv = ['vcfA2016-2016.csv', 'vcfB2016-2016.csv',
             'vcfC2016-2016.csv', 'vcfD2016-2016.csv', 
             'vcfE2016-2016.csv']
 
-def maketmp():    
+def maketmp():
     if not os.path.exists('tmp'):
         os.makedirs('tmp')
 
 def fillheader(linea):
+    ''' Crea un encabezado valido para pandas Multi-level index
+    '''
     lista = linea.split(';')
     for i in range(2,len(lista),2):
         lista[i] = lista[i-1] #Repite Nombre AFP
@@ -24,15 +26,20 @@ def fillheader(linea):
     return ';'.join(lista)
 
 def cleancsv(filecsv):
+    ''' Separa en archivos csv validos
+    '''
     file_name, file_ext = filecsv.split('.')
     
     with open('rawdata/'+filecsv, 'r') as f_in:
         lines = f_in.readlines()
-    
+    # Enumera lineas en blanco que separan trozos de datos
+    # Trozos tienen distintas AFP encabezados no homogeneos
     blanks = [i for (i, s) in enumerate(lines) if s == "\n"]
+    # Crea carpeta temporal
     maketmp()
     
     for j in range(len(blanks)):
+        # Crea archivos csv validos con trozos
         f_out = open('tmp/'+file_name+'_%s.'%j+file_ext, 'w')
         
         if j < len(blanks)-1:
@@ -50,9 +57,13 @@ def cleancsv(filecsv):
         
         f_out.close()
 
-if clean_hist:
-    for filecsv in histcsv:
-        cleancsv(filecsv)
-if clean_year:
-    for filecsv in yearcsv:
-        cleancsv(filecsv)
+def main():
+    if clean_hist:
+        for filecsv in histcsv:
+            cleancsv(filecsv)
+    if clean_year:
+        for filecsv in yearcsv:
+            cleancsv(filecsv)
+
+if __name__ == "__main__":
+    main()
