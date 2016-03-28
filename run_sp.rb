9d5a7dcd604348e0f7c45bf0016a3518d728f368
jrovegno <javier.rovegno@gmail.com>
2016-03-28 20:51:47 -0300

    created test_sp and changed run_sp

--- run_sp.rb
+++ run_sp.rb
@@ -1,20 +1,9 @@
-require 'fileutils'
-require_relative 'spensiones'
+# Run all the scripts
 
-db = Spensiones.new
-db.vc_excel(2016, 2016, 'A')
-today = Time.now
-day = 60*60*24
+#~ load './vc_historical.rb'
+load './vc_this_year.rb'
+load './vc_this_month.rb'
+%x( python cleancsv.py )
+%x( python curator.py )
 
-descargas = Dir.pwd + '/' + db.a0.conf.outdir + '/'
-df = db.vc_df((today - 2*day).strftime("%Y-%m-%d"), 'A')
-print db.vc_table(1, 1)
-df.write_csv(descargas + 'data.csv')
-
-#~ index = Daru::DateTimeIndex.date_range(
-  #~ :start => '2016-2-1', :periods => 20, :freq => 'D')
-
-print "db list of methods: " + (db.methods - Object.methods).to_s + "\n"
-print "db.a0 list of methods: " + (db.a0.methods - Object.methods).to_s + "\n"
-
-Webdrone.irb_console
+#~ Webdrone.irb_console
---
+++ test_sp.rb
@@ -0,0 +1,17 @@
+require 'fileutils'
+require_relative 'spensiones'
+
+db = Spensiones.new
+db.vc_excel(2016, 2016, 'A')
+today = Time.now
+day = 60*60*24
+
+descargas = Dir.pwd + '/' + db.a0.conf.outdir + '/'
+df = db.vc_df((today - 2*day).strftime("%Y-%m-%d"), 'A')
+print db.vc_table(1, 1)
+df.write_csv(descargas + 'data.csv')
+
+print "db list of methods: " + (db.methods - Object.methods).to_s + "\n"
+print "db.a0 list of methods: " + (db.a0.methods - Object.methods).to_s + "\n"
+
+Webdrone.irb_console
