fc0774d108dafdbfcb9c32193b282fa2c0649b1c
jrovegno <javier.rovegno@gmail.com>
2016-03-29 14:03:13 -0300

    Added yesno to run_sp

--- run_sp.rb
+++ run_sp.rb
@@ -1,12 +1,24 @@
 # Run all the scripts
 
-#~ load './vc_historical.rb'
-load './vc_this_year.rb'
-load './vc_this_month.rb'
-%x( python cleancsv.py )
-%x( python curator.py )
-%x( jupyter nbconvert --to notebook --execute A-E_AFP.ipynb )
-%x( jupyter nbconvert --to notebook --execute ValoresCuotaAFP.ipynb )
+def yesno(prompt = 'Continue?', default = 'y')
+    alt = 'n' if default == 'y'
+    alt = 'y' if default == 'n'
+    q = "#{prompt}  [#{default}]/#{alt}: "
+    input = [(print q), gets.rstrip.downcase][1]
+    input = default if input.empty?
+    return input == 'y'
+end
+run_hist = yesno("Update historical?", 'n')
+run_year = yesno("Update this year?", 'n')
+run_month = yesno("Update this month?", 'y')
+run_python = yesno("Curate data?", 'y')
+run_nb = yesno("Update notebooks?", 'y')
 
+load './vc_historical.rb' if run_hist
+load './vc_this_year.rb' if run_year
+load './vc_this_month.rb' if run_month
+%x( python cleancsv.py ) if run_python
+%x( python curator.py ) if run_python
+%x( jupyter nbconvert --to notebook --execute A-E_AFP.ipynb ) if run_nb
+%x( jupyter nbconvert --to notebook --execute ValoresCuotaAFP.ipynb ) if run_nb
 
-#~ Webdrone.irb_console
