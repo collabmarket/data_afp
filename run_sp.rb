cbe21a3937a272245fcc68ff77cddd7b3d792b75
jrovegno <javier.rovegno@gmail.com>
2016-03-29 00:40:11 -0300

    run_sp ruby,python y notebooks

--- run_sp.rb
+++ run_sp.rb
@@ -5,5 +5,8 @@ load './vc_this_year.rb'
 load './vc_this_month.rb'
 %x( python cleancsv.py )
 %x( python curator.py )
+%x( jupyter nbconvert --to notebook --execute A-E_AFP.ipynb )
+%x( jupyter nbconvert --to notebook --execute ValoresCuotaAFP.ipynb )
+
 
 #~ Webdrone.irb_console
