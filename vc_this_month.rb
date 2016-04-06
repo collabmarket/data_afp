39638e0fc5c7a25cd522ef3a48a3e64bccc63472
jrovegno <javier.rovegno@gmail.com>
2016-04-06 02:13:20 -0300

    refactoring vc_this_month date_range

--- vc_this_month.rb
+++ vc_this_month.rb
@@ -6,7 +6,7 @@ d = 60*60*24
 today = Time.now
 year = today.year
 month = today.month
-
+dby = (today - 2*d).day # day before yesterday
 # Check last data
 month_file = './rawdata/month_data.csv'
 if File.exist?(month_file)
@@ -16,30 +16,30 @@ if File.exist?(month_file)
   lastday = aux['Fecha'][-1]
   lyear, lmonth, lday = lastday.split('-').map(&:to_i)
 else
-  lyear, lmonth, lday = [0, 0, 0]
-end
-# Si month_file tiene datos del mes los usa
-if year == lyear and month == lmonth
-  # Lista de dias posteriores a month_file
-  ## TODO revisar casos extremos
-  days = (lday + 1)..(today - 2*d).day if today.day > 2
-# En otro caso crea month_file desde dia 1
-else
-  days = 1..(today - 2*d).day if today.day > 2
   # Check and create df from two days ago
   aux = db.vc_df((today - 2*d).strftime("%Y-%m-%d"), 'A')
   # Delete data from aux only left headers
   for i in 3..8
     aux.delete_row(i)
   end
+  lyear, lmonth, lday = [year, month, 0]
+end
+
+begin
+  days = Daru::DateTimeIndex.date_range(
+  :start => DateTime.new(lyear,lmonth,lday+1), 
+  :end   => DateTime.new(year,month,dby), :freq => 'D').to_a
+rescue Exception
+  FileUtils.rm_f(month_file)
+  exit(0)
 end
 
 fondos = ['A', 'B', 'C', 'D', 'E']
 
 # Crea df para todos los fondos del presente mes
-for  f in fondos
+for f in fondos
   for day in days
-    df = db.vc_df("#{year}-#{month}-#{day}", f)
+    df = db.vc_df(day.strftime("%Y-%m-%d"), f)
     aux = aux.concat(df)
   end
 end
@@ -52,4 +52,5 @@ aux.write_csv(descargas + 'month_data.csv')
 # Copia df valores cuota presente mes en rawdata
 FileUtils.cp(descargas + 'month_data.csv', rawdata)
 
-#~ Webdrone.irb_console
+# Remueve month_file, si cubre mas de un mes
+FileUtils.rm_f(month_file) if days.length > 32 
