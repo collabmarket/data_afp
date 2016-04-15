ea23eca5118b3ca9215783f026e6b7fd7d94dd84
jrovegno <javier.rovegno@gmail.com>
2016-04-15 03:18:35 -0300

    Improve test_sp

--- test_sp.rb
+++ test_sp.rb
@@ -8,10 +8,14 @@ day = 60*60*24
 
 descargas = Dir.pwd + '/' + db.a0.conf.outdir + '/'
 df = db.vc_df((today - 2*day).strftime("%Y-%m-%d"), 'A')
-print db.vc_table(1, 1)
+puts db.vc_table(1, 1)
+puts 'df: Read from web'
 df.write_csv(descargas + 'data.csv')
-
-print "db list of methods: " + (db.methods - Object.methods).to_s + "\n"
-print "db.a0 list of methods: " + (db.a0.methods - Object.methods).to_s + "\n"
+puts 'dfcsv: Read from csv'
+dfcsv = Daru::DataFrame.from_csv descargas + 'data.csv'
+puts "db list of methods: " + (db.methods - Object.methods).to_s + "\n"
+puts "db.a0 list of methods: " + (db.a0.methods - Object.methods).to_s + "\n"
+puts 'db.a0.quit: Close browser'
+puts 'exit: Exit console'
 
 Webdrone.irb_console
