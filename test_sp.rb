42517d955927dcd6fbcc25be587b978ca579415d
jrovegno <javier.rovegno@gmail.com>
2016-04-15 16:31:12 -0300

    Added comments in test_sp

--- test_sp.rb
+++ test_sp.rb
@@ -9,12 +9,13 @@ day = 60*60*24
 descargas = Dir.pwd + '/' + db.a0.conf.outdir + '/'
 df = db.vc_df((today - 2*day).strftime("%Y-%m-%d"), 'A')
 puts db.vc_table(1, 1)
-puts 'df: Read from web'
+puts 'df: Readed from web'
 df.write_csv(descargas + 'data.csv')
-puts 'dfcsv: Read from csv'
+puts 'dfcsv: Readed from csv'
 dfcsv = Daru::DataFrame.from_csv descargas + 'data.csv'
 puts "db list of methods: " + (db.methods - Object.methods).to_s + "\n"
 puts "db.a0 list of methods: " + (db.a0.methods - Object.methods).to_s + "\n"
+puts "Files downloaded in : " + descargas
 puts 'db.a0.quit: Close browser'
 puts 'exit: Exit console'
 
