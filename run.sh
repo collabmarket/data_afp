8d14698234d48e40de77a1919665286e756b5871
jrovegno <javier.rovegno@gmail.com>
2016-04-20 22:46:38 -0300

    Added nbconvert in runn.sh

--- run.sh
+++ run.sh
@@ -10,3 +10,9 @@ ruby vc_this_year.rb  2>&1 | tee -a ${LOG_FILE}
 ruby vc_this_month.rb  2>&1 | tee -a ${LOG_FILE}
 python cleancsv.py  2>&1 | tee -a ${LOG_FILE}
 python curator.py 2>&1 | tee -a ${LOG_FILE}
+jupyter nbconvert --to notebook --execute ValoresCuotaCapital.ipynb --output ValoresCuotaCapital.ipynb 2>&1 | tee -a ${LOG_FILE}
+jupyter nbconvert --to notebook --execute ValoresCuotaCuprum.ipynb --output ValoresCuotaCuprum.ipynb 2>&1 | tee -a ${LOG_FILE}
+jupyter nbconvert --to notebook --execute ValoresCuotaHabitat.ipynb --output ValoresCuotaHabitat.ipynb 2>&1 | tee -a ${LOG_FILE}
+jupyter nbconvert --to notebook --execute ValoresCuotaModelo.ipynb --output ValoresCuotaModelo.ipynb 2>&1 | tee -a ${LOG_FILE}
+jupyter nbconvert --to notebook --execute ValoresCuotaPlanvital.ipynb --output ValoresCuotaPlanvital.ipynb 2>&1 | tee -a ${LOG_FILE}
+jupyter nbconvert --to notebook --execute ValoresCuotaProvida.ipynb --output ValoresCuotaProvida.ipynb 2>&1 | tee -a ${LOG_FILE}
