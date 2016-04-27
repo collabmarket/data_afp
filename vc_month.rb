3eb245dc68d4f45cb463e7b2b618a21985c53ebd
jrovegno <javier.rovegno@gmail.com>
2016-04-27 20:25:10 -0300

    Refactored vc_month use lastday_sp

--- vc_month.rb
+++ vc_month.rb
@@ -6,21 +6,24 @@ puts "[INFO]--" + Time.now.strftime('%Y-%m-%d %H:%M:%S') + "--" +
      "vc_month" + "--" +"INIT"
 
 db = Spensiones.new
-today = DateTime.parse(Time.now.utc.to_s) # date_range same timezone
 lastday_sp = db.vc_last('A')
-year = lastday_sp.year
+lmonth_sp = lastday_sp.month
+lyear_sp = lastday_sp.year
 
 # Check last data
 rawdata = Dir.pwd + '/rawdata/'
 month_file = rawdata + 'month_data.csv'
-year_file = rawdata + "vcfA#{year}-#{year}.csv"
+# TODO: Caso principio de agno
+year_file = rawdata + "vcfA#{lyear_sp}-#{lyear_sp}.csv"
 
 if File.exist?(year_file)
-  # Read last line of year_file split and extract last
-  day = IO.readlines(year_file)[-1].split(";")[0]
-  year_lastday = DateTime.strptime(day, '%Y-%m-%d')
+  # Read last line of year_file split and extract last day
+  d = IO.readlines(year_file)[-1].split(";")[0]
+  # Parse last day of year_file
+  year_lastday = DateTime.strptime(d, '%Y-%m-%d')
 else
-  year_lastday = DateTime.new(today.year,today.month,1)
+  # Si no existe year_file ultimo dia mes anterior con datos spensiones
+  year_lastday = DateTime.new(lyear_sp,lmonth_sp,1) - 1
 end
  
 if File.exist?(month_file)
@@ -39,23 +42,26 @@ if File.exist?(month_file)
   # Fecha primera y ultima linea de month_file
   firstday = DateTime.strptime(aux['Fecha'][0], '%Y-%m-%d')
   lastday = DateTime.strptime(aux['Fecha'][-1], '%Y-%m-%d')
+
   # Caso month_file tiene datos de mas de un mes
   # Elimina datos mes pasado cuando se agregan en vc_year
-  if (year_lastday - firstday).to_i > 0
+  if (year_lastday - firstday).to_i >= 0
     FileUtils.rm_f(month_file)
     aux = db.vc_df_head(lastday_sp, 'A')
-    # inicio mes
-    inicio = DateTime.new(today.year,today.month,1)
+    # inicio mes con datos spensiones
+    inicio = DateTime.new(lyear_sp,lmonth_sp,1)
+
   # Caso month_file se puede actualizar
   else
     # inicio ultimo dia month_file mas 1
     inicio = DateTime.new(lastday.year,lastday.month,lastday.day+1)
   end
+
+# Caso no existe month_file
 else
-  # Caso no existe month_file
   aux = db.vc_df_head(lastday_sp, 'A')
   # inicio mes
-  inicio = DateTime.new(today.year,today.month,1)
+  inicio = DateTime.new(lyear_sp,lmonth_sp,1)
 end
 
 # Actualiza ultimo dia
