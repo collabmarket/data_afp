4790bbb24ea176498cd5174c3711814bad81197b
jrovegno <javier.rovegno@gmail.com>
2016-04-26 23:10:09 -0300

    Fixed update last day

--- vc_this_month.rb
+++ vc_this_month.rb
@@ -9,10 +9,12 @@ today = DateTime.parse(Time.now.utc.to_s) # date_range same timezone
 lastday_sp = db.vc_last('A')
 
 # Check last data
-month_file = './rawdata/month_data.csv'
+rawdata = Dir.pwd + '/rawdata/'
+month_file = rawdata + 'month_data.csv'
+
 if File.exist?(month_file)
   # Warning: from_csv change headers order
-  aux = Daru::DataFrame.from_csv './rawdata/month_data.csv'
+  aux = Daru::DataFrame.from_csv month_file
   # Busca datos incompletos
   aux_nan = aux.filter(:row) do |row|
             row.to_a.include? "-- "
@@ -27,6 +29,7 @@ if File.exist?(month_file)
   firstday = DateTime.strptime(aux['Fecha'][0], '%Y-%m-%d')
   lastday = DateTime.strptime(aux['Fecha'][-1], '%Y-%m-%d')
   # Caso month_file tiene datos de mas de un mes
+  # TODO: Eliminar datos mes pasado cuando se agregan en vc_year
   if (lastday-firstday).to_i >= 31
     FileUtils.rm_f(month_file)
     aux = db.vc_df_head(lastday_sp, 'A')
@@ -44,12 +47,26 @@ else
   inicio = DateTime.new(today.year,today.month,1)
 end
 
-begin
-  days = Daru::DateTimeIndex.date_range(
-  :start => inicio, :end => lastday_sp, :freq => 'D').to_a
-rescue Exception
-  # Caso month_file actualizado
+# Actualiza ultimo dia
+if (lastday_sp - inicio).to_i == 0
+  days = [lastday_sp]
+# Caso month_file actualizado
+elsif (lastday_sp - inicio).to_i == -1
   days = []
+  puts "[INFO]--" + Time.now.strftime('%Y-%M-%d %H:%M:%S') + "--" + 
+       "this_month" + "--" + "UPDATED"
+else
+  begin
+    # Caso month_file varios dias
+    days = Daru::DateTimeIndex.date_range(
+           :start => inicio, :end => lastday_sp, 
+           :freq => 'D').to_a
+  rescue Exception
+    # Si existe un error no actualiza 
+    days = []
+    puts "[INFO]--" + Time.now.strftime('%Y-%M-%d %H:%M:%S') + "--" + 
+         "this_month" + "--" + "FAILED"
+  end
 end
 
 fondos = ['A', 'B', 'C', 'D', 'E']
@@ -63,7 +80,6 @@ for f in fondos
 end
 
 descargas = Dir.pwd + '/' + db.a0.conf.outdir + '/'
-rawdata = Dir.pwd + '/rawdata/'
 
 # Escribe csv con valores cuota presente mes
 aux.write_csv(descargas + 'month_data.csv')
