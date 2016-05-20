db6600d6930e1e699c63232afd3ad518bff5e382
jrovegno <javier.rovegno@gmail.com>
2016-05-20 19:46:19 -0400

    Clean run tee

--- run.sh
+++ run.sh
@@ -6,18 +6,20 @@ cd $(dirname "$0")
 LOG_FILE="spension.log"
 DATE=$(date +%Y-%m-%d)
 
-git pull 2>&1 | tee -a ${LOG_FILE}
-ruby vc_historical.rb 2>&1 | tee -a ${LOG_FILE}
-ruby vc_year.rb  2>&1 | tee -a ${LOG_FILE}
-ruby vc_month.rb  2>&1 | tee -a ${LOG_FILE}
-python cleancsv.py  2>&1 | tee -a ${LOG_FILE}
-python curator.py 2>&1 | tee -a ${LOG_FILE}
-jupyter nbconvert --to notebook --execute ValoresCuotaCapital.ipynb --output ValoresCuotaCapital.ipynb 2>&1 | tee -a ${LOG_FILE}
-jupyter nbconvert --to notebook --execute ValoresCuotaCuprum.ipynb --output ValoresCuotaCuprum.ipynb 2>&1 | tee -a ${LOG_FILE}
-jupyter nbconvert --to notebook --execute ValoresCuotaHabitat.ipynb --output ValoresCuotaHabitat.ipynb 2>&1 | tee -a ${LOG_FILE}
-jupyter nbconvert --to notebook --execute ValoresCuotaModelo.ipynb --output ValoresCuotaModelo.ipynb 2>&1 | tee -a ${LOG_FILE}
-jupyter nbconvert --to notebook --execute ValoresCuotaPlanvital.ipynb --output ValoresCuotaPlanvital.ipynb 2>&1 | tee -a ${LOG_FILE}
-jupyter nbconvert --to notebook --execute ValoresCuotaProvida.ipynb --output ValoresCuotaProvida.ipynb 2>&1 | tee -a ${LOG_FILE}
-git add rawdata/*.csv data/*.csv *.ipynb 2>&1 | tee -a ${LOG_FILE}
-git commit -m "Update data and nb $DATE" 2>&1 | tee -a ${LOG_FILE}
-git push 2>&1 | tee -a ${LOG_FILE}
+{
+git pull
+ruby vc_historical.rb
+ruby vc_year.rb
+ruby vc_month.rb
+python cleancsv.py
+python curator.py
+jupyter nbconvert --to notebook --execute ValoresCuotaCapital.ipynb --output ValoresCuotaCapital.ipynb
+jupyter nbconvert --to notebook --execute ValoresCuotaCuprum.ipynb --output ValoresCuotaCuprum.ipynb
+jupyter nbconvert --to notebook --execute ValoresCuotaHabitat.ipynb --output ValoresCuotaHabitat.ipynb
+jupyter nbconvert --to notebook --execute ValoresCuotaModelo.ipynb --output ValoresCuotaModelo.ipynb
+jupyter nbconvert --to notebook --execute ValoresCuotaPlanvital.ipynb --output ValoresCuotaPlanvital.ipynb
+jupyter nbconvert --to notebook --execute ValoresCuotaProvida.ipynb --output ValoresCuotaProvida.ipynb
+git add rawdata/*.csv data/*.csv *.ipynb
+git commit -m "Update data and nb $DATE"
+git push
+} 2>&1 | tee -a ${LOG_FILE}
