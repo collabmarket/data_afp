a200617d4c644c92cff736278f81983eea418187
jrovegno <javier.rovegno@gmail.com>
2017-02-17 17:13:03 -0300

    datatoolbox.py use basename

--- datatoolbox.py
+++ datatoolbox.py
@@ -6,7 +6,7 @@ def logg_info(msg, tipo='INFO', status='OK'):
     timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     print("[%s]--%s--%s--%s" % (tipo, timestamp, msg, status))
 
-def makedir(dirname, by=__file__):
+def makedir(dirname, by=os.path.basename(__file__)):
     if not os.path.exists(dirname):
         os.makedirs(dirname)
         logg_info('%s mkdir %s' % (by, dirname), tipo='INFO', status='OK')
