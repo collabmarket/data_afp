e2ef780bfec93b47ec9f0f8e55bf00f8808a1ac2
jrovegno <javier.rovegno@gmail.com>
2017-02-19 19:55:58 -0300

    Refactored vc_historical

--- vc_historical.rb
+++ vc_historical.rb
@@ -1,14 +1,21 @@
 require 'fileutils'
 require_relative 'spensiones'
+require_relative 'datatoolbox'
 
-# Exec init
-puts "[INFO]--" + Time.now.strftime('%Y-%m-%d %H:%M:%S') + "--" + 
-     "vc_historical" + "--" +"INIT"
+# Exec INIT
+thisfile = File.basename(__FILE__)
+logg_info("#{thisfile}", tipo='INFO', status='INIT')
 
+# Variables
 db = Spensiones.new
 lastyear = Time.now.year - 1
 fondos = ['A', 'B', 'C', 'D', 'E']
 inityear = {'A' => 2002, 'B' => 2002, 'C' => 1981, 'D' => 2002, 'E' => 2000}
+
+# Nombres directorios
+descargas = Dir.pwd + '/' + db.a0.conf.outdir + '/'
+rawdata = Dir.pwd + '/rawdata/'
+tmp = Dir.pwd + '/tmp'
 
 # Descarga excel valores cuota historicos
 for f in fondos
@@ -18,15 +25,8 @@ end
 # Espera 10 seg para que se descargen los archivos
 db.a0.wait.time        10
 
-descargas = Dir.pwd + '/' + db.a0.conf.outdir + '/'
-rawdata = Dir.pwd + '/rawdata/'
-
-#Crea directorio rawdata
-if not File.exist?(rawdata)
-  FileUtils.mkdir(rawdata)
-  puts "[INFO]--" + Time.now.strftime('%Y-%m-%d %H:%M:%S') + "--" +
-  "vc_historical mkdir rawdata" + "--" +"OK"
-end
+#Crea directorio rawdata si no existe
+makedir(rawdata, by=thisfile)
 
 # Copia excel valores cuota historicos en rawdata
 for f in fondos
@@ -34,20 +34,14 @@ for f in fondos
 end
 
 # Crea carpeta tmp
-if not File.exist?('tmp')
-  FileUtils.mkdir('tmp')
-  puts "[INFO]--" + Time.now.strftime('%Y-%m-%d %H:%M:%S') + "--" +
-  "vc_historical mkdir tmp" + "--" +"OK"
-end
+makedir(tmp, by=thisfile)
 
 # Indica a cleancsv recrear archivos historicos
 FileUtils.touch('tmp/historical')
-puts "[INFO]--" + Time.now.strftime('%Y-%m-%d %H:%M:%S') + "--" +
-     "vc_historical touch msg to cleancsv" + "--" +"OK"
+logg_info("#{thisfile} touch msg to cleancsv", tipo='INFO', status='OK')
 
 # Close Browser
 db.a0.quit
 
-# Exec ok
-puts "[INFO]--" + Time.now.strftime('%Y-%m-%d %H:%M:%S') + "--" + 
-     "vc_historical" + "--" +"DONE"
+# Exec DONE
+logg_info("#{thisfile}", tipo='INFO', status='DONE')
